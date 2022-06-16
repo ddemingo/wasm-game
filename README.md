a9ad6e0b8b481bfbc7aa6e92c8820fb66826cbb9
David de Mingo <david@optersoft.com>
2022-06-16 11:33:47 +0200

    app compiles and renders a triangle

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Initialize the project:
 
 ```sh
-$ npm init rust-webpack
+$ npm init rust-webpack 'your-package-name'
 ```
 
 Install dependencies and wasm-pack:
@@ -16,8 +16,39 @@ $ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 After the installation completes, you can now run a development server:
 
 ```sh
-$ npm run start
+$ npm start
 ```
 
-When you see ｢wdm｣: Compiled successfully. , you can browse your app
+When you see `｢wdm｣: Compiled successfully.` , you can browse your app
 at http://localhost:8080.
+
+## How to run in debug mode
+
+```sh
+# Builds the project and opens it in a new browser tab. Auto-reloads when the project changes.
+npm start
+```
+
+## How to build in release mode
+
+```sh
+# Builds the project and places it into the `dist` folder.
+npm run build
+```
+
+## How to run unit tests
+
+```sh
+# Runs tests in Firefox
+npm test -- --firefox
+
+# Runs tests in Chrome
+npm test -- --chrome
+
+# Runs tests in Safari
+npm test -- --safari
+```
+
+## References
+
+* https://rustwasm.github.io/docs/wasm-pack/tutorials/hybrid-applications-with-webpack/using-your-library.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::JsCast;
 
+mod sierpinski;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,18 +11,50 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    // https://h3manth.com/content/html5-canvas-full-screen-and-full-page
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    context.move_to(300.0, 0.0); // top of triangle
+    context.begin_path();
+    context.line_to(0.0, 600.0); // bottom left of triangle
+    context.line_to(600.0, 600.0); // bottom right of triangle
+    context.line_to(300.0, 0.0); // back to top of triangle
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
+}
+
+fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: u8) {
+    draw_triangle(&context, points);
+}
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
 }
